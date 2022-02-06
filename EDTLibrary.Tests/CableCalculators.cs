fc8197b02b1e4aacff79ddc80013d7675ca2376d
Patrick <pdeault@gmail.com>
2022-02-06 13:13:52 -0600

    Add EdtLibrary Test Project and sample test

---
+++ EDTLibrary.Tests/CableCalculators.cs
@@ -0,0 +1,21 @@
+﻿using EDTLibrary;
+using EDTLibrary.Calculators;
+using EDTLibrary.Models;
+using Xunit;
+
+namespace EDTLibrary.Tests
+{
+    public class CableCalculators
+    {
+        [Fact]
+        public void ReturnValidConductorsQty()
+        {
+            var calc = new ConductorQtyCalculator();
+            var load = new LoadModel();
+
+            calc.Calculate(load);
+
+            Assert.Equal(2, 2);
+        }
+    }
+}
