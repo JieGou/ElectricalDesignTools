bbcda497d0693ed760d563d661dab6e756279010
Patrick <pdeault@gmail.com>
2022-07-15 16:43:05 -0500

    Update resource files

---
+++ ExcelLibraryCore/ExcelHelper.cs
@@ -0,0 +1,341 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Excel = Microsoft.Office.Interop.Excel;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace ExcelLibrary
+{
+    public class ExcelHelper
+    {
+        private readonly bool _visible;
+        private readonly bool _displayAlerts;
+        private readonly bool _screenUpdating;
+
+        public ExcelHelper(bool visible = true, bool displayAlerts = false, bool screenUpdating = true)
+        {
+            _visible = visible;
+            _displayAlerts = displayAlerts;
+            _screenUpdating = screenUpdating;
+        }
+
+        public Excel.Application App { get; set; }
+        public Excel.Range Rng { get; set; }
+        public Excel.Workbooks Wbks { get; set; }
+        public Excel.Workbook Wbk { get; set; }
+        public Excel.Worksheet Sheet { get; set; }
+        public Excel.ListObject Tbl { get; set; }
+
+        public int MinColumnWidth { get; set; } = 5;
+        public string DefaultTableStyle { get; set; } = "TableStyleLight18";
+        public void Initialize(string sheetName = "Sheet1")
+        {
+            App = new Excel.Application();
+            App.Visible = _visible;
+            App.DisplayAlerts = _displayAlerts;
+            App.ScreenUpdating = _screenUpdating;
+
+            Wbks = App.Workbooks; ;
+            Wbk = Wbks.Add();
+            Sheet = Wbk.Sheets["Sheet1"];
+
+            if (sheetName != "Sheet1") {
+                Sheet.Name = sheetName;
+            }
+            
+        }
+
+        public void AddSheet(string sheetName)
+        {
+            if (Sheet.Name!="Sheet1") {
+                Sheet = Wbk.Sheets.Add(Missing.Value, Sheet);
+
+            }
+            Sheet.Name= sheetName;
+
+            //System.Runtime.InteropServices.Marshal.ReleaseComObject(sht);
+            //sht = null;
+        }
+
+        public async void ExportTest(string fileLocation = "C:\\temp\\", string filename = "Edt Excel Test")
+        {
+            try {
+
+                for (int i = 0; i < 10; i++) {
+                    int rowNum = i + 1;
+
+                    for (int j = 0; j < 10; j++) {
+                        int colNum = j + 1;
+
+                        Sheet.Cells[rowNum, colNum] = $"{rowNum}, {colNum}";
+                        if (rowNum == 1) {
+                            Sheet.Cells[rowNum, colNum].Interior.Color = Excel.XlRgbColor.rgbPaleVioletRed;
+                        }
+                        if (rowNum == 2) {
+                            Sheet.Cells[rowNum, colNum].Interior.Color = System.Drawing.Color.FromArgb(150, 254, 254);
+                        }
+                    }
+                }
+
+                Sheet.UsedRange.AutoFit();
+
+                System.Diagnostics.Debug.WriteLine("Write Complete. " + DateTime.Now.ToString("HH:mm:ss"));
+            }
+            catch (Exception ex) {
+                string errMsg = "Error Saving Excel File - " + ex.Message;
+                System.Diagnostics.Debug.WriteLine(errMsg);
+                //MessageBox.Show(errMsg);
+
+                Release();
+                //or
+                //CollectGarbage();
+                //or
+                //KillExcelAsync();
+            }
+        }
+
+        /// <summary>
+        /// Exports the property names and values of an object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="classObject"></param>
+        /// <param name="propertiesToIgnore"></param>
+        public void ExportObjectProperties<T>(T classObject, List<string> propertiesToIgnore) where T : class, new()
+        {
+            try {
+                var objectProperties = classObject.GetType().GetProperties();
+                List<string> propsToWrite = new List<string>();
+
+
+                Sheet.Cells[1, 1] = "Obeject Export Report";
+                Range rng = Sheet.Range["A1:F1"];
+                rng.Merge();
+
+
+                int row = 3;
+                int col = 1;
+                //Property Names
+                bool addProp;
+                foreach (var prop in objectProperties) {
+                    addProp = true;
+                    foreach (var propToIgnore in propertiesToIgnore) {
+                        if (prop.Name == propToIgnore) {
+                            addProp = false;
+                        }
+                    }
+                    if (addProp == true) {
+                        propsToWrite.Add(prop.Name);
+                    }
+
+                }
+
+                foreach (var prop in propsToWrite) {
+                    Sheet.Cells[row, col] = $"{prop}";
+                    col += 1;
+                }
+
+                Sheet.UsedRange.Columns.AutoFit();
+                foreach (Range column in Sheet.UsedRange.Columns) {
+                    if (column.ColumnWidth < 5) {
+                        column.ColumnWidth = 5;
+                    }
+                }
+
+                Tbl = (Excel.ListObject)Sheet.ListObjects.AddEx(
+                        Source: (Range)Sheet.Range[Sheet.Cells[3, 1], Sheet.Cells[3, propsToWrite.Count]],
+                        XlListObjectHasHeaders: Excel.XlYesNoGuess.xlYes);
+
+                //Tbl = Sheet.ListObjects.AddEx(Source: (Range)Sheet.Range[Sheet.Cells[3, 1], Sheet.Cells[3, propsToWrite.Count]]);
+                Tbl.Name = "TableTest";
+                Tbl.TableStyle = "TableStyleLight18";
+
+            }
+            catch (Exception ex) {
+
+                string errMsg = "Error writing data to excel - " + ex.Message;
+                System.Diagnostics.Debug.WriteLine(errMsg);
+                //MessageBox.Show(errMsg);
+                //Release();            }
+
+            }
+        }
+
+        /// <summary>
+        /// Exports a list of objects with property names as column headers.
+        /// </summary>
+        /// <typeparam name="T">Generic Mapper Object created based of a model object</typeparam>
+        /// <param name="list"></param>
+        /// <param name="propertiesToInclude"></param>
+        /// <param name="startRow"></param>
+        /// <param name="reportName"></param>
+        public void ExportListOfObjects<T>(List<T> list, List<string> propertiesToInclude, int startRow = 3, string reportName = "Object Export Report") where T : class//, new()
+        {
+            try {
+              
+                var objectProperties = list[0].GetType().GetProperties();
+                List<string> propsToWrite = new List<string>();
+
+                Sheet.Cells[1, 1] = reportName;
+                Range rng = Sheet.Range["A1:F1"];
+                rng.Merge();
+
+
+                int row = startRow;
+                int col = 1;
+
+                //Create Property List
+                bool addProp = false;
+                foreach (var prop in objectProperties) {
+                    addProp = false;
+                    foreach (var propToInclude in propertiesToInclude) {
+                        if (prop.Name == propToInclude) {
+                            addProp = true;
+                        }
+                    }
+                    if (addProp == true) {
+                        propsToWrite.Add(prop.Name);
+                    }
+
+                }
+
+                //Column Headers
+                foreach (var propName in propsToWrite) {
+                    Sheet.Cells[row, col] = propName;
+                    col += 1;
+                }
+
+                //Property Values
+                foreach (var item in list) { 
+                    row += 1;
+                    col = 1;
+                    foreach (var prop in objectProperties) {
+                        foreach (var propToWrite in propsToWrite) {
+                            if (prop.Name == propToWrite) {
+                                Sheet.Cells[row, col] = prop.GetValue(item);
+                                col += 1;
+                            }
+                        }
+                    }
+                }
+
+                //Auto-Fit Columns with minimum width
+                Sheet.UsedRange.Columns.AutoFit();
+                foreach (Range column in Sheet.UsedRange.Columns) {
+                    if (column.ColumnWidth < MinColumnWidth) {
+                        column.ColumnWidth = MinColumnWidth; //property in this class
+                    }
+                }
+
+                //Format data as a table.
+                Tbl = (Excel.ListObject)Sheet.ListObjects.AddEx(
+                        Source: (Range)Sheet.Range[Sheet.Cells[3, 1], Sheet.Cells[row, propsToWrite.Count]],
+                        XlListObjectHasHeaders: Excel.XlYesNoGuess.xlYes);
+
+                Tbl.Name = reportName.Replace(" ",""); 
+                Tbl.TableStyle = DefaultTableStyle; //property in this class
+
+            }
+            catch (Exception ex) {
+
+                string errMsg = "Error writing data to excel - " + ex.Message;
+                System.Diagnostics.Debug.WriteLine(errMsg);
+                //MessageBox.Show(errMsg);
+                //Release();            }
+
+            }
+        }
+
+        public void SetVisibility(bool isVisible)
+        {
+            App.Visible = isVisible;
+        }
+        public async Task SaveWorkbook(string fileLocation = "C:\\temp\\", string filename = "Edt Excel Test")
+        {
+
+            try {
+                var filePath = fileLocation + filename + DateTime.Now.ToString().Replace(":", "_");
+                Wbk.SaveAs(filePath,
+                    Missing.Value, Missing.Value,
+                    Missing.Value, Missing.Value,
+                    Missing.Value,
+                    Excel.XlSaveAsAccessMode.xlNoChange,
+                    Missing.Value, Missing.Value,
+                    Missing.Value, Missing.Value,
+                    Missing.Value);
+
+                System.Diagnostics.Debug.WriteLine("Save Complete. " + DateTime.Now.ToString("HH:mm:ss"));
+            }
+            catch (Exception ex) {
+                string errMsg = "Error Saving Excel File - " + ex.Message;
+                System.Diagnostics.Debug.WriteLine(errMsg);
+                //MessageBox.Show(errMsg);
+                //Release();
+            }
+
+        }
+        public void Release()
+        {
+
+            //must release each object that referenced a COM object
+
+            if (Tbl != null) {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(Tbl);
+                Tbl = null;
+            }
+
+            if (Sheet != null) {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(Sheet);
+                Sheet = null;
+            }
+            if (Wbk != null) {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(Wbk);
+                Wbk = null;
+            }
+
+            if (Wbks != null) {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(Wbks);
+                Wbks = null;
+            }
+
+            if (App != null) {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(App);
+                App = null;
+            }
+        }
+        public async Task CollectGarbage()
+        {
+            //await Task.Delay(5000);
+            //Process[] excelProcesses = Process.GetProcessesByName("excel");
+            //foreach (Process p in excelProcesses) {
+            //    if (string.IsNullOrEmpty(p.MainWindowTitle)) // use MainWindowTitle to distinguish this excel process with other excel processes 
+            //    {
+            //        p.Kill();
+            //    }
+
+            //}
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+        private static async Task KillExcelAsync()
+        {
+            await Task.Delay(5000);
+            Process[] excelProcesses = Process.GetProcessesByName("excel");
+            foreach (Process p in excelProcesses) {
+                if (string.IsNullOrEmpty(p.MainWindowTitle)) // use MainWindowTitle to distinguish this excel process with other excel processes 
+                {
+                    p.Kill();
+                }
+
+                //}
+
+            }
+        }
+    }
+}
---
+++ ExcelLibraryCore/HelperExcelInterop.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace ExcelLibrary
+{
+    public class HelperExcelInterop
+    {
+        public static async void WriteToExcel(string fileLocation = "C:\\temp\\", string filename = "Edt Excel Test")
+        {
+            object oMissing = Missing.Value;
+
+            Excel.Application xlApp = null;
+            Excel.Range xlRange = null;
+            Excel.Workbooks xlBooks = null;
+            Excel.Workbook xlBook = null;
+            Excel.Worksheet xlSheet = null;
+
+            int worksheetCount = 0;
+
+            try {
+                //Initialize
+
+                xlApp = new Excel.Application();
+                xlApp.DisplayAlerts = false;
+                xlApp.Visible = true;
+                //excelApp.UserControl = false;
+                //excelApp.Calculation = Excel.XlCalculation.xlCalculationManual;
+                //excelApp.ScreenUpdating = false;
+
+                //Create 
+                xlBooks = xlApp.Workbooks;
+                xlBook = xlBooks.Add();
+                xlSheet = xlBook.Sheets["Sheet1"];
+
+                for (int i = 0; i < 10; i++) {
+                    int rowNum = i + 1;
+
+                    for (int j = 0; j < 10; j++) {
+                        int colNum = j + 1;
+
+                        xlSheet.Cells[rowNum, colNum] = $"{rowNum}, {colNum}";
+                        if (rowNum == 1) {
+                            xlSheet.Cells[rowNum, colNum].Interior.Color = Excel.XlRgbColor.rgbPaleVioletRed;
+                        }
+                        if (rowNum == 2) {
+                            xlSheet.Cells[rowNum, colNum].Interior.Color = System.Drawing.Color.FromArgb(150, 254, 254);
+                        }
+                        
+                    }
+                }
+
+                //enable
+                //excelApp.Calculation = Excel.XlCalculation.xlCalculationManual;
+                //excelApp.ScreenUpdating = true;
+
+                //save Workbook - if file exists, overwrite it
+                var filePath = fileLocation + filename + DateTime.Now.ToString().Replace(":", "_");
+                xlBook.SaveAs(filePath,
+                    Missing.Value, Missing.Value,
+                    Missing.Value, Missing.Value,
+                    Missing.Value,
+                    Excel.XlSaveAsAccessMode.xlNoChange,
+                    Missing.Value, Missing.Value,
+                    Missing.Value, Missing.Value,
+                    Missing.Value);
+
+
+                System.Diagnostics.Debug.WriteLine("Status: Complete. " + DateTime.Now.ToString("HH:mm:ss"));
+            }
+            catch (Exception ex) {
+                string errMsg = "Error Saving Excel File - " + ex.Message;
+                System.Diagnostics.Debug.WriteLine(errMsg);
+                //MessageBox.Show(errMsg);
+            }
+
+            finally {
+                //if (xlSheet != null) {
+                //    System.Runtime.InteropServices.Marshal.FinalReleaseComObject(xlSheet);
+                //    xlSheet = null;
+
+                //}
+                //if (xlBook != null) {
+                //    xlBook.Close(Type.Missing, Type.Missing, Type.Missing);
+                //    System.Runtime.InteropServices.Marshal.FinalReleaseComObject(xlBook);
+                //    xlBook = null;
+
+                //}
+
+                //if (xlApp != null) {
+                //    xlApp.Quit();
+                //    System.Runtime.InteropServices.Marshal.FinalReleaseComObject(xlApp);
+                //    xlApp = null;
+                //}
+
+
+
+
+                //must release each object that referenced a COM object
+                ReleaseExcel(ref xlApp, ref xlBooks, ref xlBook, ref xlSheet);
+
+                //await KillExcelAsync();
+
+            }
+        }
+
+        private static void ReleaseExcel(ref Excel.Application xlApp, ref Workbooks xlBooks, ref Workbook xlBook, ref Worksheet xlSheet)
+        {
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlSheet);
+            xlSheet = null;
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlBook);
+            xlBook = null;
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlBooks);
+            xlBooks = null;
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlApp);
+            xlApp = null;
+        }
+
+        private static async Task KillExcelAsync()
+        {
+            //await Task.Delay(5000);
+            //Process[] excelProcesses = Process.GetProcessesByName("excel");
+            //foreach (Process p in excelProcesses) {
+            //    if (string.IsNullOrEmpty(p.MainWindowTitle)) // use MainWindowTitle to distinguish this excel process with other excel processes 
+            //    {
+            //        p.Kill();
+            //    }
+
+            //}
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
