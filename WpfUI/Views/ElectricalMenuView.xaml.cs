f2e140701eb45290a5c3ab804ab2bfb75dc301bc
Patrick <pdeault@gmail.com>
2022-07-16 18:36:55 -0500

    misc

--- WpfUI/Views/ElectricalMenuView.xaml.cs
+++ WpfUI/Views/ElectricalMenuView.xaml.cs
@@ -1,23 +1,6 @@
-﻿using EDTLibrary;
-using EDTLibrary.DataAccess;
-using EDTLibrary.Models.DistributionEquipment;
-using EDTLibrary.Models.Loads;
-using EDTLibrary.TestDataFolder;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Input;
-using System.Windows.Media;
-using WpfUI.Helpers;
+﻿using System.Windows.Controls;
 using WpfUI.ViewModels;
-using WpfUI.Views.SubViews;
+using WpfUI.Views.Electrical.MjeqSubviews;
 
 namespace WpfUI.Views;
 
@@ -27,9 +10,6 @@ namespace WpfUI.Views;
 public partial class ElectricalMenuView : UserControl
 {
     private ElectricalMenuViewModel elecVm { get { return DataContext as ElectricalMenuViewModel; } }
-
-    DteqTabsView _dteqDetailsView = new DteqTabsView();
-    LoadDetailView _loaDetailView = new LoadDetailView();
 
     public ElectricalMenuView()
     {
