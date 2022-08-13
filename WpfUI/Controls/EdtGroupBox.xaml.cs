733a55277cf15a162648aec397f665337a210bf7
rimctto <77760487+rimctto@users.noreply.github.com>
2022-08-13 02:32:44 -0500

    Finish Edt GroupBox

--- WpfUI/Controls/EdtGroupBox.xaml.cs
+++ WpfUI/Controls/EdtGroupBox.xaml.cs
@@ -23,6 +23,18 @@ public partial class EdtGroupBox : UserControl
 
 
 
+    public string Header
+    {
+        get { return (string)GetValue(HeaderProperty); }
+        set { SetValue(HeaderProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Header.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty HeaderProperty =
+        DependencyProperty.Register("Header", typeof(string), typeof(EdtGroupBox), new PropertyMetadata("GroupBox Header"));
+
+
+
     public Brush HeaderForeGround
     {
         get { return (Brush)GetValue(HeaderForeGroundProperty); }
@@ -31,7 +43,7 @@ public partial class EdtGroupBox : UserControl
 
     // Using a DependencyProperty as the backing store for HeaderForeGround.  This enables animation, styling, binding, etc...
     public static readonly DependencyProperty HeaderForeGroundProperty =
-        DependencyProperty.Register("HeaderForeGround", typeof(Brush), typeof(EdtGroupBox), new PropertyMetadata(Brushes.Transparent));
+        DependencyProperty.Register("HeaderForeGround", typeof(Brush), typeof(EdtGroupBox), new PropertyMetadata(Brushes.Black));
 
 
 
@@ -43,7 +55,7 @@ public partial class EdtGroupBox : UserControl
 
     // Using a DependencyProperty as the backing store for HeaderBackground.  This enables animation, styling, binding, etc...
     public static readonly DependencyProperty HeaderBackgroundProperty =
-        DependencyProperty.Register("HeaderBackground", typeof(Brush), typeof(EdtGroupBox), new PropertyMetadata(Brushes.Transparent));
+        DependencyProperty.Register("HeaderBackground", typeof(Brush), typeof(EdtGroupBox), new PropertyMetadata(Brushes.LightBlue));
 
 
 
@@ -56,6 +68,20 @@ public partial class EdtGroupBox : UserControl
     // Using a DependencyProperty as the backing store for ContentBackground.  This enables animation, styling, binding, etc...
     public static readonly DependencyProperty ContentBackgroundProperty =
         DependencyProperty.Register("ContentBackground", typeof(Brush), typeof(EdtGroupBox), new PropertyMetadata(Brushes.Transparent));
+
+
+
+
+    public object Content
+    {
+        get { return (object)GetValue(ContentProperty); }
+        set { SetValue(ContentProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Content.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty ContentProperty =
+        DependencyProperty.Register("Content", typeof(object), typeof(EdtGroupBox), new PropertyMetadata(new object()));
+
 
 
 
